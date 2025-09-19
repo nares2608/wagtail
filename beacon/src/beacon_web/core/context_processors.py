c8d933b67aef4c4931f31877952d86a3a753dffc
NARESH KUMAR M <nares2608@gmail.com>
2025-09-19 14:09:10 +0530

    Delete beacon/src directory

--- beacon/src/beacon_web/core/context_processors.py
+++
@@ -1,9 +0,0 @@
-from core.models.snippets import Menus, FooterMenu, FAQ, Service, Package, Testimonial
- 
-def global_context(request):
-    return {
-        "menus": Menus.objects.all(),
-        "footermenus": FooterMenu.objects.all(),
-        
-
-    }
--- beacon/src/beacon_web/home/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .home import HomePage
-from .llc import LLCPage
-from .ccorp import CCorpPage
-from .scorp import SCorpPage
-from .nonprofit import NonProfitPage
